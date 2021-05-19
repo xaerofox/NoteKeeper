6a4ee27cda798a2f0de7404a32e9319178c89ee7
Jose Torres <xaerofox@gmail.com>
2021-05-19 13:19:41 -0400

    Implemented recyclerViewAdapter functionality

--- app/src/main/java/com/example/notekeeper/NoteListActivity.kt
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.kt
@@ -10,21 +10,30 @@ import com.example.notekeeper.databinding.ActivityNoteListBinding
 class NoteListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNoteListBinding
-    private var contentBinding = binding.layoutContentNoteList
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNoteListBinding.inflate(layoutInflater)
+        var contentBinding = binding.layoutContentNoteList
         val view = binding.root
         setContentView(view)
         setSupportActionBar(binding.toolbar)
 
-        binding.fab.setOnClickListener { view ->
+        binding.fab.setOnClickListener {
             val activityIntent = Intent(this, MainActivity::class.java)
             startActivity(activityIntent)
         }
 
         contentBinding.listItems.layoutManager = LinearLayoutManager(this)
+
+        contentBinding.listItems.adapter = NoteRecyclerAdapter(this, DataManager.notes)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        var contentBinding = binding.layoutContentNoteList
+        contentBinding.listItems.adapter?.notifyDataSetChanged()
     }
 
 }
