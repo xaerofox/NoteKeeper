055291261ca80fa3928d1063b5232b7865789e3c
Jose Torres <xaerofox@gmail.com>
2021-05-20 15:50:19 -0400

    Updated manifest to launch new activity Modified ItemsActivity to utilize appropriate viewbinding Added hamburger icon into new nav activity's toolbar Modified back button functionality to close nav drawer when it is opened Commented out some boilerplate code

--- app/src/main/java/com/example/notekeeper/ItemsActivity.kt
+++ app/src/main/java/com/example/notekeeper/ItemsActivity.kt
@@ -1,7 +1,9 @@
 package com.example.notekeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -12,45 +14,74 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.notekeeper.databinding.ActivityItemsBinding
 
 class ItemsActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityItemsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_items)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        binding = ActivityItemsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        var contentBinding = binding.layoutAppBar.layoutContentItems
+
+        val toolbar = binding.layoutAppBar.toolbar
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        binding.layoutAppBar.fab
+            .setOnClickListener { view ->
+            startActivity(Intent(this, MainActivity::class.java))
         }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        contentBinding.listItems.layoutManager = LinearLayoutManager(this)
+        contentBinding.listItems.adapter = NoteRecyclerAdapter(this, DataManager.notes)
+        
+        val drawerLayout = binding.drawerLayout
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.openNav, R.string.closeNav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+//        val navView: NavigationView = findViewById(R.id.nav_view)
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+//            ), drawerLayout
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.items, menu)
-        return true
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.items, menu)
+//        return true
+//    }
+//
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+//    }
+
+    override fun onResume() {
+        super.onResume()
+        var contentBinding = binding.layoutAppBar.layoutContentItems
+        contentBinding.listItems.adapter?.notifyDataSetChanged()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START))
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        else super.onBackPressed()
     }
 }
