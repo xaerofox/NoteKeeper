f92c58918dd714352638529f5a36063d9ab737cb
Jose Torres <xaerofox@gmail.com>
2021-05-25 13:57:08 -0400

    Tested out recording espresso test

---
+++ app/src/androidTest/java/com/example/notekeeper/ItemsActivityTest.kt
@@ -0,0 +1,76 @@
+package com.example.notekeeper
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.filters.LargeTest
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class ItemsActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityScenarioRule(ItemsActivity::class.java)
+
+    @Test
+    fun itemsActivityTest() {
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.listItems),
+                childAtPosition(
+                    withId(R.id.layoutContentItems),
+                    0
+                )
+            )
+        )
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
+
+        val actionMenuItemView = onView(
+            allOf(
+                withId(R.id.action_next), withContentDescription("Next"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.toolbar),
+                        1
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        actionMenuItemView.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
